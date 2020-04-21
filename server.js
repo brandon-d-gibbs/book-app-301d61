dcfa378126a7f3ca2bbf7c4e239da2811c0edd19
lamsharkie <gibbs.brandon.d@gmail.com>
2020-04-21 11:27:28 -0700

    added basic setup and proof of life.

---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict'
+
+// Keep stuff hidden
+require('dotenv').config();
+
+// Let's plug it in
+const express = require('express');
+const app = express();
+
+// *** Dependencies ***
+// const superagent = require('superagent');
+
+// Get EJS plugged in
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+
+// PORT
+const PORT = process.env.PORT || 3015
+
+// *** Routes ***
+app.get('/', renderHome);
+
+
+// *** Callback Functions ***
+function renderHome(request, response){
+    response.status(200).render('./pages/index.ejs');
+}
+
+
+
+
+
+
+// Turn it on
+app.listen(PORT, () => {
+    console.log(`Hey, you like books? Let\'s start looking for some. In case you need to know, we\'re doing this on ${PORT}`);
+})
